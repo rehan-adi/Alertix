473d4ccc2aac3cc75210f8315d47f5ced8244b28
Rehan <rehanalire52@gmail.com>
2024-12-17 11:39:44 +0530

    Add database connection with r2d2 and diesel

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,24 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 use std::env;
 
+mod config;
+mod models;
 mod routes;
 mod schema;
-mod models;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
+    let pool = config::db::db_connect();
 
     let port = env::var("PORT").expect("Failed to get PORT");
     println!("Server is running on {}", port);
 
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .service(routes::loan::loan)
             .service(routes::account::account)
             .service(routes::transaction::transaction)
